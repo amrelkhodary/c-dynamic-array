dfadfd78d156fad947454df6a8ed84b7cbd76716
amrelkhodary <amrelkhodarybusiness@gmail.com>
2025-12-22 10:21:16 +0200

    in ./headers: added cdyar.h; in ./makefile: modified makefile to accomodate newly added cdyar.h

---
+++ headers/cdyar.h
@@ -0,0 +1,14 @@
+#ifndef H_CDYAR
+    #define H_CDYAR
+
+    //a quality-of-life single header file that included all other header files
+    //some of these inclusions are not neccessary because they are
+    //automatically included by other header files, but having them doesn't hurt and
+    //help with clarity.
+    #include "./cdyar_darray.h"
+    #include "./cdyar_error.h"
+    #include "./cdyar_structures.h"
+    #include "./cdyar_types.h"
+    #include "./cdyar_arithmetic.h"
+
+#endif
